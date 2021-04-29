dcb9da60b21b29285c328d42665fc60a3603b943
Francesco Uccelli <39736679+Flrotm@users.noreply.github.com>
2021-04-29 15:54:11 -0500

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-p = 20
+p = 4
 
 w=np.random.rand(p)
 
@@ -16,7 +16,7 @@ plt.plot(x_ds, y_ds, '*')
 
 def h(x, w, p):
     suma = 0
-    for j in range(p):
+    for j in range(0,p):
         suma += w[j]*(x**j)
     return suma
     #return 1/(1+np.exp((-w)*(x-p)))
@@ -31,15 +31,16 @@ def mae(x, y, w):
 def grad_mse(x, y, w):
     grad_w = np.zeros(p)
     for j in range(len(w)):
-        grad_w[j] = sum([e[0]-h(e[1], w, p)*((-e[1]**j)) for e in zip(y, x)]) / (2*len(y))
+        grad_w[j] = sum([(e[0]-h(e[1], w, p))*(-e[1]**j) for e in zip(y, x)]) / (2*len(y))
+        #print(grad_w[j])
     return grad_w
 
 y_pd = [h(xi, w, p) for xi in x_ds]
 plt.plot(x_ds, y_pd)
 
-alpha = 0.007
+alpha = 0.7
 
-for i in range(1000):
+for i in range(10000):
     grad_w = grad_mse(x_ds, y_ds, w)
     for j in range(len(w)):
         w[j] = w[j] - alpha*grad_w[j]
